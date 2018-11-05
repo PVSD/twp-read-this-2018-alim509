0ddb4a2033bbefe96dc91f9fe94291f32ce07763
al5091 <angelovinolim@gmail.com>
2018-11-05 11:21:31 -0500

    idk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import sun.tools.java.Imports;
+
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("Imports");
+        Scanner fileScanner = new Scanner(file);
+        fileScanner.close();
+
+        int maxIndx = -1;
+        String text[] = new String[1000];
+
+        while (fileScanner.hasNext())
+        {
+            maxIndx++;
+            text[maxIndx] = fileScanner.nextLine();
+        }
+
+        fileScanner.close();
+
+        for(int j = 0; j <= maxIndx; j++)
+        {
+            System.out.println( text[j] );
+        }
     }
 }
